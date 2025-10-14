1d66b40cdb58bca6e88b6a6a67f1ade6c7cbc681
mahbub045 <mahbubrmn45@gmail.com>
2025-10-14 15:21:46 +0600

    Update employee selection label and format adviser names in ActiveCases and Cases components

--- src/Components/General/Dashboard/CommonComponents/Cases/ActiveCases/ActiveCases.tsx
+++ src/Components/General/Dashboard/CommonComponents/Cases/ActiveCases/ActiveCases.tsx
@@ -292,7 +292,7 @@ const ActiveCases: React.FC = () => {
                 <Card className="shadow-lg bg-light-success rounded-3 p-3 mt-3 mb-3">
                   <Row className="justify-content-center g-3">
                     <Col xs="12" sm="6" md="3">
-                      <Label>Select Employee</Label>
+                      <Label>Select Case Created Employee</Label>
                       <Input
                         type="select"
                         id="employeeFilter"
@@ -305,7 +305,16 @@ const ActiveCases: React.FC = () => {
                         <option value="">All Employee</option>
                         {adviserData?.map((adviser: AdviserInfoProps) => (
                           <option key={adviser.alias} value={adviser.user.id}>
-                            {adviser.user.first_name} {adviser.user.last_name}
+                            {adviser.user.title
+                              ? adviser.user.title[0].toUpperCase() +
+                                adviser.user.title.slice(1).toLowerCase() +
+                                " "
+                              : ""}
+                            {adviser.user.first_name}{" "}
+                            {adviser.user.middle_name
+                              ? adviser.user.middle_name + " "
+                              : ""}
+                            {adviser.user.last_name}
                           </option>
                         ))}
                       </Input>
