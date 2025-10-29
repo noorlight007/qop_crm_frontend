058f69f55df936d221964bbf3174e32b4d9ed726
mahbub045 <mahbubrmn45@gmail.com>
2025-10-29 11:52:17 +0600

    feat: Integrate LoadingSpinner component for improved loading state in Notes

--- src/Components/General/Dashboard/CommonComponents/SingleCaseInfo/Components/CaseDetails/Components/Notes/Notes/Notes.tsx
+++ src/Components/General/Dashboard/CommonComponents/SingleCaseInfo/Components/CaseDetails/Components/Notes/Notes/Notes.tsx
@@ -1,3 +1,4 @@
+import LoadingSpinner from "@/app/loading";
 import { useGetNotesQuery } from "@/Redux/Reducers/CommonComponents/SingleCaseInfo/CaseDetails/Notes/NotesApi";
 import { NoteProps } from "@/Types/CommonComponents/SingleCaseInfo/CaseDetails/NotesTypes";
 import { formatDateToDMYAndTime } from "@/utils/dateAndTimeFormatter";
@@ -171,7 +172,7 @@ const Notes: React.FC = () => {
             {isLoading ? (
               <tr>
                 <td colSpan={TABLE_COLUMNS.length} className="text-center">
-                  Loading...
+                  <LoadingSpinner />
                 </td>
               </tr>
             ) : notesData &&
