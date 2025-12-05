4f188b175fccb781c53e2bd515c211ac14e819d5
mahbub045 <mahbubrmn45@gmail.com>
2025-12-05 16:59:13 +0600

    feat: Enhance Copy Case modal with form handling and API integration

--- src/Redux/Reducers/CommonComponents/SingleCaseInfo/CaseCopy/CaseCopyApi.ts
+++ src/Redux/Reducers/CommonComponents/SingleCaseInfo/CaseCopy/CaseCopyApi.ts
@@ -3,9 +3,10 @@ import { baseApi } from "@/Redux/Api/BaseApi";
 export const CaseCopyApi = baseApi.injectEndpoints({
   endpoints: (builder) => ({
     copyCase: builder.mutation({
-      query: ({ case_alias }) => ({
+      query: ({ case_alias, payload }) => ({
         url: `/cases/${case_alias}/clone/`,
         method: "POST",
+        body: payload,
       }),
       invalidatesTags: ["CaseCopy"],
     }),
