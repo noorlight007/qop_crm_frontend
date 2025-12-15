9a3878217208e4b263ea52430ccfeab6df755bf0
mahbub045 <mahbubrmn45@gmail.com>
2025-12-15 11:13:09 +0600

    fix: Correct date format in getTodayDate function to YYYY-MM-DD

--- src/Data/Layout/HeaderData.tsx
+++ src/Data/Layout/HeaderData.tsx
@@ -12,7 +12,7 @@ const getTodayDate = () => {
   const day = String(today.getDate()).padStart(2, "0");
   const month = String(today.getMonth() + 1).padStart(2, "0");
   const year = today.getFullYear();
-  return `${day}-${month}-${year}`;
+  return `${year}-${month}-${day}`;
 };
 
 export const notificationData = [
