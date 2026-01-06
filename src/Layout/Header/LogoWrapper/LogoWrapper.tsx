948c163095c0730422d052e3b662e59c1259f76b
mahbub045 <mahbubrmn45@gmail.com>
2026-01-06 16:47:22 +0600

    feat: Replace LogoChanger with LogoAndFavIconChanger component and update logo source in LogoWrapper
    - Added LogoAndFavIconChanger component to manage logo and favicon changes.
    - Removed the obsolete LogoChanger component.
    - Updated AppearanceContainer to use LogoAndFavIconChanger.
    - Modified LogoWrapper to dynamically fetch logo source from appearance data.

--- src/Layout/Header/LogoWrapper/LogoWrapper.tsx
+++ src/Layout/Header/LogoWrapper/LogoWrapper.tsx
@@ -1,12 +1,15 @@
 import SVG from "@/CommonComponent/SVG";
 import { ImagePath } from "@/Constant";
 import { useAppDispatch, useAppSelector } from "@/Redux/Hooks";
+import { useGetAppranceQuery } from "@/Redux/Reducers/Appearance/AppearanceApi";
 import { setSideBarToggle } from "@/Redux/Reducers/ThemeCustomizerReducer";
 import Image from "next/image";
 
 const LogoWrapper = () => {
   const { sideBarToggle } = useAppSelector((state) => state.themeCustomizer);
   const dispatch = useAppDispatch();
+  const { data: appearanceData } = useGetAppranceQuery(undefined);
+  const logoSrc = appearanceData?.logo || `${ImagePath}/logo/logo1.png`;
   return (
     <div className="logo-wrapper d-flex align-items-center col-auto">
       <Image
@@ -14,16 +17,18 @@ const LogoWrapper = () => {
         height={60}
         priority
         className="light-logo img-fluid"
-        src={`${ImagePath}/logo/logo1.png`}
+        src={logoSrc}
         alt="logo"
+        style={{ width: "120px", height: "50px" }}
       />
       <Image
         width={90}
         height={30}
         priority
         className="dark-logo img-fluid"
-        src={`${ImagePath}/logo/logo-dark.png`}
+        src={logoSrc}
         alt="logo"
+        style={{ width: "120px", height: "50px" }}
       />
       <a
         className="close-btn toggle-sidebar"
