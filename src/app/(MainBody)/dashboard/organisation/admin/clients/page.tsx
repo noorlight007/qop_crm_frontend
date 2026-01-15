b261702a4922f529212e7383b921ce72d2081150
mahbub045 <mahbubrmn45@gmail.com>
2026-01-15 11:20:05 +0600

    Refactor Clients and Leads components to restrict delete button visibility based on user roles; add OrganisationAdminClientsContainer and OrganisationAdminClients page for better organization management

---
+++ src/app/(MainBody)/dashboard/organisation/admin/clients/page.tsx
@@ -0,0 +1,21 @@
+"use client";
+import { FunctionComponent, useEffect, useState } from "react";
+
+const OrganisationAdminClients = () => {
+  const [MyAwesomeMap, setClient] = useState<FunctionComponent>();
+  useEffect(() => {
+    (async () => {
+      if (typeof window !== "undefined") {
+        const newClient = (
+          await import(
+            "@/Components/General/Dashboard/Organisation/Admin/Cases/Clients"
+          )
+        ).default;
+        setClient(() => newClient);
+      }
+    })();
+  }, []);
+  return MyAwesomeMap ? <MyAwesomeMap /> : "";
+};
+
+export default OrganisationAdminClients;
