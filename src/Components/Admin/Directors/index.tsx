427abf6884ce773f6d9268f443ca5dc9925aff3a
Nasif Wasek Fahim <nasif.wasek@gmail.com>
2026-01-26 18:03:57 +0600

    created common authuser for directors

--- src/Components/Admin/Directors/index.tsx
+++ src/Components/Admin/Directors/index.tsx
@@ -1,8 +1,22 @@
+import Breadcrumbs from "@/Components/Common/Breadcrumbs/Breadcrumbs";
+import { Container, Row } from "reactstrap";
+import DirectorList from "./DirectorList/DirectorList";
+
 const AdminDirectorsContainer: React.FC = () => {
   return (
-    <div>
-      {/* JSX here */}
-    </div>
+    <>
+      <Breadcrumbs
+        title="Director List"
+        subTitle="Welcome back! Check all the Network Directors"
+        parent="Client"
+        child="Dashboard"
+      />
+      <Container fluid>
+        <Row>
+            <DirectorList />
+        </Row>
+      </Container>
+    </>
   );
 };
 
