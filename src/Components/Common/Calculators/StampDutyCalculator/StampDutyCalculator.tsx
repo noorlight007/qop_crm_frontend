bb1bb452ac7c1fa717d3daf07f03736b5f715412
mahbub045 <mahbubrmn45@gmail.com>
2026-02-06 15:08:02 +0600

    feat: add Overpayment and Stamp Duty calculators with container components and routing

---
+++ src/Components/Common/Calculators/StampDutyCalculator/StampDutyCalculator.tsx
@@ -0,0 +1,10 @@
+import { Container } from "reactstrap";
+import Breadcrumbs from "../../Breadcrumbs/Breadcrumbs";
+
+const StampDutyCalculator: React.FC = () => {
+  return (
+    <div></div>
+  );
+};
+
+export default StampDutyCalculator;
--- src/app/(MainBody)/organisation/director/calculators/stamp-duty-calculator/page.tsx
+++ src/app/(MainBody)/organisation/director/calculators/stamp-duty-calculator/page.tsx
@@ -1,13 +1,13 @@
 "use client";
 import { FunctionComponent, useEffect, useState } from "react";
 
-const MonthlyPaymentCalculatorPage = () => {
+const StampDutyCalculatorPage = () => {
   const [MyAwesomeMap, setClient] = useState<FunctionComponent>();
   useEffect(() => {
     (async () => {
       if (typeof window !== "undefined") {
         const newClient = (
-          await import("@/Components/Organisation/Director/Calculators/MonthlyPaymentCalculator")
+          await import("@/Components/Organisation/Director/Calculators/StampDutyCalculator")
         ).default;
         setClient(() => newClient);
       }
@@ -16,4 +16,4 @@ const MonthlyPaymentCalculatorPage = () => {
   return MyAwesomeMap ? <MyAwesomeMap /> : "";
 };
 
-export default MonthlyPaymentCalculatorPage;
+export default StampDutyCalculatorPage;
