4e6129dbf4a05d432ecc614e1fdca5231b34d60c
Nasif Wasek Fahim <nasif.wasek@gmail.com>
2026-02-09 16:27:22 +0600

    feat: implement organization management modals for updating and deleting organizations

---
+++ src/Components/Admin/Organisations/[OrganisationSlug]/OrganisationDetails/Modals/DeleteOrgModal.tsx
@@ -0,0 +1,71 @@
+
+import { useDeleteOrganisationMutation } from "@/Redux/Reducers/Common/Organisations/OrganisationDetails/SingleOrganisationApi";
+import { DeleteOrgModalProps } from "@/Types/Admin/Organisations/OrganisationTypes";
+import { useRouter } from "next/navigation";
+
+import { toast } from "react-toastify";
+import { Button, Modal, ModalBody, ModalFooter, ModalHeader } from "reactstrap";
+
+const DeleteOrgModal: React.FC<DeleteOrgModalProps> = ({
+  isOpen,
+  toggle,
+  organisationInfo,
+}) => {
+  const router = useRouter();
+  const [deleteOrganisation, { isLoading }] = useDeleteOrganisationMutation();
+  const handleDelete = async () => {
+    try {
+      const slug = organisationInfo?.organization?.slug;
+      const response = await deleteOrganisation({ slug });
+      toggle();
+      if (response.data === null) {
+        toast.success("Organisation deleted successfully!");
+      } else {
+        toast.error("Failed to delete organisation.");
+      }
+      router.push("/admin/organisations");
+    } catch (error) {
+      console.error("Failed to delete organisation", error);
+      toast.error("Failed to delete organisation. Please try again.");
+    }
+  };
+
+  return (
+    <Modal isOpen={isOpen} toggle={toggle}>
+      <ModalHeader toggle={toggle}>
+        <h3 className="text-danger">Confirm Deletion</h3>
+      </ModalHeader>
+      <ModalBody>
+        <p>
+          Are you sure you want to delete{" "}
+          <strong className="text-danger">
+            {organisationInfo?.organisation?.name}
+          </strong>{" "}
+          organisation?
+        </p>
+
+        <div className="border border-danger rounded p-3 bg-light">
+          <p className="mb-2 fw-semibold text-danger">
+            This action is irreversible.
+          </p>
+          <small className="text-muted">
+            Deleting this organisation will permanently remove all associated
+            data, including cases, leads, clients, advisers, admins, joint
+            applicants, appearance preferences, settings, profile information,
+            and all historical records. This data cannot be restored.
+          </small>
+        </div>
+      </ModalBody>
+      <ModalFooter>
+        <Button color="secondary" onClick={toggle} disabled={isLoading}>
+          Cancel
+        </Button>
+        <Button color="danger" onClick={handleDelete} disabled={isLoading}>
+          {isLoading ? "Deleting..." : "Delete"}
+        </Button>
+      </ModalFooter>
+    </Modal>
+  );
+};
+
+export default DeleteOrgModal;
