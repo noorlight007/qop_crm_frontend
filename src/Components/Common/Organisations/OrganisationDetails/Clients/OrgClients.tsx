e498c0ce14de9f376234cf6ccba68ef0ae14dd89
mahbub045 <mahbubrmn45@gmail.com>
2026-02-10 10:27:35 +0600

    refactor: update table cell structure for better alignment and readability in multiple components

--- src/Components/Common/Organisations/OrganisationDetails/Clients/OrgClients.tsx
+++ src/Components/Common/Organisations/OrganisationDetails/Clients/OrgClients.tsx
@@ -201,34 +201,36 @@ const OrgClients: React.FC = () => {
               ) : currentClients.length > 0 ? (
                 currentClients.map((client: any) => (
                   <tr key={client.alias} className="text-center">
-                    <td className="d-flex justify-content-start align-items-center gap-1 text-truncate">
-                      <span
-                        className="border rounded-circle overflow-hidden d-flex justify-content-center align-items-center"
-                        style={{ width: 40, height: 40 }}
-                      >
-                        {client?.profile_image ? (
-                          <Image
-                            src={client.profile_image}
-                            alt="Profile"
-                            width={35}
-                            height={35}
-                            className="rounded-circle"
-                          />
-                        ) : (
-                          <User size={30} className="text-primary" />
-                        )}
-                      </span>
-                      <span
-                        className="text_decoration_hover"
-                        onClick={() => toggleViewModal(client)}
-                        style={{ cursor: "pointer" }}
-                      >
-                        {client.title
-                          ? formatChoiceFieldValue(client.title)
-                          : ""}
-                        {"."} {client?.first_name} {client?.middle_name}{" "}
-                        {client?.last_name}
-                      </span>
+                    <td>
+                      <div className="d-flex justify-content-start align-items-center gap-1 text-truncate">
+                        <span
+                          className="border rounded-circle overflow-hidden d-flex justify-content-center align-items-center"
+                          style={{ width: 40, height: 40 }}
+                        >
+                          {client?.profile_image ? (
+                            <Image
+                              src={client.profile_image}
+                              alt="Profile"
+                              width={35}
+                              height={35}
+                              className="rounded-circle"
+                            />
+                          ) : (
+                            <User size={30} className="text-primary" />
+                          )}
+                        </span>
+                        <span
+                          className="text_decoration_hover"
+                          onClick={() => toggleViewModal(client)}
+                          style={{ cursor: "pointer" }}
+                        >
+                          {client.title
+                            ? formatChoiceFieldValue(client.title)
+                            : ""}
+                          {"."} {client?.first_name} {client?.middle_name}{" "}
+                          {client?.last_name}
+                        </span>
+                      </div>
                     </td>
                     <td>
                       {client?.email ? (
--- src/Components/Common/Organisations/OrganisationDetails/DangerZone/DangerZone.tsx
+++ src/Components/Common/Organisations/OrganisationDetails/DangerZone/DangerZone.tsx
@@ -18,36 +18,9 @@ const DangerZone: React.FC<FetchSingleOrganisationProps> = ({
       <Card className="shadow p-2">
         <CardHeader className="h3 text-danger">Danger Zone</CardHeader>
         <CardBody className="border-danger rounded-2 mb-4">
-          {/** Disable Organisation Protection Rules Section **/}
-          <div className="d-flex justify-content-between align-items-center mb-3">
-            <div>
-              <h5 className="fw-bold">Disable organisation protection rules</h5>
-              <p className="mb-0 opacity-75">
-                Disable organisation protection rules enforcement and APIs.
-              </p>
-            </div>
-            <Button color="danger" disabled>
-              Disable organisation protection rules
-            </Button>
-          </div>
-          <hr />
-          {/** Transfer Ownership Section **/}
-          <div className="d-flex justify-content-between align-items-center mb-3">
-            <div>
-              <h5 className="fw-bold">Transfer ownership</h5>
-              <p className="mb-0 opacity-75">
-                Transfer this organisation to another user or an organisation
-                where you have the ability to create repositories.
-              </p>
-            </div>
-            <Button color="danger" disabled>
-              Transfer
-            </Button>
-          </div>
-          <hr />
-
           {/** Delete organisation Section **/}
-          {session?.user.user_type === "NETWORK_DIRECTOR" && (
+          {(session?.user.user_type === "NETWORK_DIRECTOR" ||
+            session?.user?.user_type === "NETWORK_COMPLIANCE") && (
             <div className="d-flex justify-content-between align-items-center">
               <div>
                 <h5 className="fw-bold">Delete this organisation</h5>
