41b6036e897afe5f85b948625cf159aca246aa64
Nasif Wasek Fahim <nasif.wasek@gmail.com>
2026-02-20 11:47:55 +0600

    fix: add optional chaining for network and organisation display in AuthUsers component

--- src/Components/Admin/CommonUsers/AuthUsers/AuthUsers.tsx
+++ src/Components/Admin/CommonUsers/AuthUsers/AuthUsers.tsx
@@ -395,12 +395,12 @@ const AuthUsers: React.FC<AuthUsersProps> = ({
                     {session?.user?.user_type === "ADMIN" && (
                       <>
                         <td className="text-truncate">
-                          {user.network || (
+                          {user?.network || (
                             <small className="text-muted">Not Specified</small>
                           )}
                         </td>
                         <td className="text-truncate">
-                          {user.organisation || (
+                          {user?.organisation || (
                             <small className="text-muted">Not Specified</small>
                           )}
                         </td>
