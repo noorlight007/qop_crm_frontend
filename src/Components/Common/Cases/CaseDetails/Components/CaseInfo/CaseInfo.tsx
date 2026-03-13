0dee3729565787c074b3f8a6e346683a11b8c38a
mahbub045 <mahbubrmn45@gmail.com>
2026-03-13 12:50:19 +0600

    style: enhance case stage display with rounded background and text color

--- src/Components/Common/Cases/CaseDetails/Components/CaseInfo/CaseInfo.tsx
+++ src/Components/Common/Cases/CaseDetails/Components/CaseInfo/CaseInfo.tsx
@@ -586,7 +586,7 @@ const CaseInfo: React.FC<SingleCaseProps> = ({
                       </h6> */}
                       <h6 className="pt-1">
                         <span className="small">Case Stage:</span>{" "}
-                        <strong className="small">
+                        <strong className="small rounded-1 px-1 bg-secondary text-white">
                           {caseInfo?.case_stage
                             ? formatChoiceFieldValue(caseInfo.case_stage)
                             : "N/A"}
