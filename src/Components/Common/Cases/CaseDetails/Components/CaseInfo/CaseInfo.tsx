805e6c133a0afe3637e207fc8ee49ddc046fc04c
mahbub045 <mahbubrmn45@gmail.com>
2026-03-13 12:50:46 +0600

    refactor: remove commented-out case status display in CaseInfo component

--- src/Components/Common/Cases/CaseDetails/Components/CaseInfo/CaseInfo.tsx
+++ src/Components/Common/Cases/CaseDetails/Components/CaseInfo/CaseInfo.tsx
@@ -574,16 +574,7 @@ const CaseInfo: React.FC<SingleCaseProps> = ({
                             )}
                         </strong>
                       </h6>
-                      {/* <h6 className="pt-1">
-                        <span className="small">Case Status:</span>{" "}
-                        <strong
-                          className={`rounded-1 px-1 small ${
-                            caseInfo?.case_stage === "NOT_PROCEED" ? "bg-danger" : "bg-success"
-                          }`}
-                        >
-                          {caseInfo?.case_stage === "NOT_PROCEED" ? "Inactive" : "Active"}
-                        </strong>
-                      </h6> */}
+
                       <h6 className="pt-1">
                         <span className="small">Case Stage:</span>{" "}
                         <strong className="small rounded-1 px-1 bg-secondary text-white">
