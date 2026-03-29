54c0d804da70011b8cf1880483e88978bb5b1838
Nasif Wasek Fahim <nasif.wasek@gmail.com>
2026-03-29 14:23:39 +0600

    feat: add client enquiry components and update routing

--- src/Components/Common/CommonUsers/LeadsOrClients/Components/PublicLeadLink.tsx
+++ src/Components/Common/CommonUsers/LeadsOrClients/Components/PublicLeadLink.tsx
@@ -37,7 +37,7 @@ const PublicLeadLink: React.FC = () => {
     "",
   );
   const host = subdomain ? `${subdomain}.${baseDomain}` : baseDomain;
-  const url = host ? `https://${host}/public-enquiry` : "";
+  const url = host ? `https://${host}/client-enquiry` : "";
 
   const handleCopy = () => {
     if (!url) return;
