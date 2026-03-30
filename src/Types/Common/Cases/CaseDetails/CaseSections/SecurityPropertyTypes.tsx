196d243a4e2610360e54d9bde169c0b952f7376d
mahbub045 <mahbubrmn45@gmail.com>
2026-03-30 16:49:55 +0600

    refactor: replace 'applicant' with 'customer' in ValuationInfo component and related types

--- src/Types/Common/Cases/CaseDetails/CaseSections/SecurityPropertyTypes.tsx
+++ src/Types/Common/Cases/CaseDetails/CaseSections/SecurityPropertyTypes.tsx
@@ -79,7 +79,7 @@ export interface SecurityPropertyFormStateProps {
 
     // Valuation and Contact Details
     valuation_type: string; // "standard_val"
-    applicant: string; // "SELECT"
+    customer: string; // "SELECT"
     contact_for_access: string; // ""
     contacts_name: string; // ""
     contacts_daytime_telephone: string; // ""
@@ -183,7 +183,7 @@ export interface PropertyData {
   is_the_property_used_purely_for_residential_purposes: boolean;
   other_new_build_warranty_provider: string;
   valuation_type: string;
-  applicant: string;
+  customer: string;
   contact_for_access: string;
   contacts_name: string;
   contacts_daytime_telephone: string;
