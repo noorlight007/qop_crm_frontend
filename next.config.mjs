7d28c28719ffc62a2ef0f68513cf9478f5682ca0
mahbub045 <mahbubrmn45@gmail.com>
2026-05-19 12:42:49 +0600

    feat: Enhance security headers and add new icons for PWA

--- next.config.mjs
+++ next.config.mjs
@@ -21,7 +21,15 @@ const nextConfig = {
           {
             key: "Content-Security-Policy",
             value:
-              "default-src 'self'; img-src 'self' https://api.staging.qopcrm.com https://api.qopcrm.com data: https:; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; connect-src 'self' https://api.staging.qopcrm.com https://api.qopcrm.com;",
+              "default-src 'self'; frame-ancestors 'none'; img-src 'self' https://api.staging.qopcrm.com https://api.qopcrm.com data: https:; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; style-src-elem 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' data: https://fonts.gstatic.com; connect-src 'self' https://api.staging.qopcrm.com https://api.qopcrm.com wss://api.staging.qopcrm.com wss://api.qopcrm.com;",
+          },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
+            key: "X-Frame-Options",
+            value: "DENY",
           },
         ],
       },
